3eeb782d1bc42c245bdeaf195ab48317dd0a7854
Danai Kravariti <danai.kravariti@solera.com>
2022-10-31 16:09:17 +0200

    basic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="SkyviewerApp">
-      <header className="SkyviewerApp-header">
-        
+    <div>
+      <header>
+        <h1>SKYVIEWER</h1>
+        <h2>Search your flights easily</h2>
+        <form>
+          <d>Origin:</d>
+          <select name="origin">
+            <option value="bronx">Bronx</option>
+            <option value="brooklyn">Brooklyn</option>
+            <option value="manhattan">Manhattan</option>
+            <option value="queens">Queens</option>
+            <option value="statenisland">Staten Island</option>
+          </select>
+          <d>Destination:</d>
+          <select name="destination">
+            <option value="bronx">Bronx</option>
+          </select>
+          <h3></h3>
+          <label>One way:</label>
+          <input type="checkbox"></input>
+          <h3></h3>
+          <input type="submit"></input>
+        </form>
       </header>
     </div>
   );
