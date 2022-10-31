699149d4609f23d7a9cb6d06baa4dd8ee46fca06
Danai Kravariti <danai.kravariti@solera.com>
2022-10-31 15:19:56 +0200

    Add project files.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="SkyviewerApp">
+      <header className="SkyviewerApp-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
