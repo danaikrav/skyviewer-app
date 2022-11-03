5132eded03fe5fc645ad7d6341950f17bfaeb456
Danai Kravariti <danai.kravariti@solera.com>
2022-11-03 20:19:53 +0200

    search api kinda working

--- src/App.js
+++ src/App.js
@@ -1,46 +1,27 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom"
 import "./App.css";
 import Header from "./components/UI/Header";
 import SearchForm from "./components/Search Engine/SearchForm";
 import Flights from "./components/Flights/Flights";
 import NewUser from "./components/User Form/NewUser";
+import Api from "./Api";
 
-import axios from "axios";
+function App () {
+  
+  
 
-const api = axios.create({
-  baseURL: "http://localhost:8080",
-});
 
-class App extends Component {
-  state = {
-    origins: [],
-  };
-
-  constructor() {
-    super();
-    this.getOrigins();
-  }
-
-  getOrigins = async () => {
-    let data = await api.get("/flights/origins").then(({ data }) => data);
-    this.setState ({ origins: data })
-
-  };
-
-  render() {
     return (
       <div className="App">
         <Header></Header>
-        <SearchForm
-          origins={this.state.origins.map((origin) => (
-            <h2>{origin}</h2>
-          ))}
-        ></SearchForm>
-        <Flights id="hide"/>
-        <NewUser id="hide"/>
+        <SearchForm></SearchForm>
+        
+        <Flights/>
+        <NewUser/>
+        
       </div>
     );
-  }
 }
 
 export default App;
--- src/components/Search Engine/SearchForm.js
+++ src/components/Search Engine/SearchForm.js
@@ -1,22 +1,61 @@
 import "./SearchForm.css";
-import { useState } from "react";
+//import { useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react";
+import Api from "../../Api";
 
-const SearchForm = (props)  => {
+const SearchForm = () =>{
   const [isChecked, SetIsChecked] = useState(true);
-  const [origin, SetOrigin] = useState("");
+  const [destinations, setDestinations] = useState([]);
+  const [origins, setOrigins] = useState([]);
+  const [origin, setOrigin] = useState("default");
+  const [destination, setDestination] = useState("default");
+  const [departDate, setDepartDate] = useState(null);
+  const [arriveDate, setArriveDate] = useState(null);
+  const [flightInfo, setFlightInfo] = useState({});
+  //const navigate = useNavigate();
+
+  useEffect(()  => {
+    const getOrigins = async () => {
+      try{
+        const response = await Api.get("/flights/origins");
+      setOrigins(response.data);
+      console.log(response.data);
+    } catch (err) {
+      // Not in the 200 response range
+      console.log(err.response.data)
+    }
+    };
+    getOrigins();
+  }, []);
+
+  useEffect(()  => {
+    const getDestinations = async () => {
+      try{
+        const response = await Api.get("/flights/origins/"+"Paris"+"");
+      setDestinations(response.data);
+      console.log(response.data);
+    } catch (err) {
+      // Not in the 200 response range
+      console.log(err.response.data)
+    }
+    };
+    getDestinations();
+  }, []);
   
   const originHandler = (event) => {
-    document.getElementById('origin').addEventListener('change', function() {
-      console.log('You selected: ', this.value);
-    });
+    setOrigin(event.target.value);
+    console.log(origin);
   }
 
-  const DestinationChangeHandler = (event) => {
-    if (event.target.value !== "default") {
+  const destinationHandler = (event) => {
+    setDestination(event.target.value);
+    if (event.target.value!=="default") {
       document.getElementById("oneway-box").style.display = "block";
     } else {
       document.getElementById("oneway-box").style.display = "none";
     }
+    console.log(destination);
+
   };
 
   const DatesHandler = (event) => {
@@ -28,8 +67,21 @@ const SearchForm = (props)  => {
     SetIsChecked((current) => !current);
   };
 
+  const departHandler = (event) => {
+    setDepartDate(event.target.value);
+    console.log(departDate);
+
+  }
+
+  const arriveHandler = (event) => {
+    setArriveDate(event.target.value);
+  }
+
   const sumbitHandler = (event) => {
-    
+    event.preventDefault();
+    setFlightInfo({origin: origin, destination: destination, departDate: departDate, arriveDate: arriveDate})
+    console.log(flightInfo);
+    //navigate.push("/");
   }
 
   return (
@@ -39,21 +91,24 @@ const SearchForm = (props)  => {
         <label> From: </label>
         <select onChange={originHandler} id="origin" className="input-box" name="origin">
           <option value="default">--- Please select ---</option>
-          <option id="1" value={props.origins[0]}>{props.origins[0]}</option>
-          <option value={props.origins[1]}>{props.origins[1]}</option>
-          <option value={props.origins[2]}>{props.origins[2]}</option>
-          <option value={props.origins[3]}>{props.origins[3]}</option>
-          <option value={props.origins[4]}>{props.origins[4]}</option>
+          <option id="1" value={origins[0]}>{origins[0]}</option>
+          <option value={origins[1]}>{origins[1]}</option>
+          <option value={origins[2]}>{origins[2]}</option>
+          <option value={origins[3]}>{origins[3]}</option>
+          <option value={origins[4]}>{origins[4]}</option>
         </select>
         <label > To: </label>
         <select
           className="input-box"
-          onChange={DestinationChangeHandler}
+          onChange={destinationHandler}
           name="destination"
         >
           <option value="default">--- Please select ---</option>
-          <option value="bronx">Seville</option>
-          <option value="bronx">Athens</option>
+          <option value={destinations[0]}>{destinations[0]}</option>
+          <option value={destinations[1]}>{destinations[1]}</option>
+          <option value={destinations[2]}>{destinations[2]}</option>
+          <option value={destinations[4]}>{destinations[4]}</option>
+          <option value={destinations[5]}>{destinations[5]}</option>
         </select>
         <p> </p>
         <div className="hide" id="oneway-box">
@@ -66,12 +121,12 @@ const SearchForm = (props)  => {
           <p> </p>
           <div id="depart-date">
             <label>Depart At:</label>
-            <input className="input-box" type="date" min="2022-11-02"></input>
+            <input onChange={departHandler} className="input-box" type="date" min="2022-11-02"></input>
           </div>
 
           <div className="hide" id="arrive-date">
             <label>Arrive At:</label>
-            <input className="input-box" type="date"></input>
+            <input onChange={arriveHandler} className="input-box" type="date"></input>
           </div>
         </div>
         <p> </p>
